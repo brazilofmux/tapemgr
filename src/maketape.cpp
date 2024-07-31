3cd7d1b2bce5768eac28cac68435a0182bba1276
Stephen Dennis <brazilofmux@gmail.com>
2024-07-31 03:51:11 -0600

    Throw exception if configuration file doesn't exist.

--- src/maketape.cpp
+++ src/maketape.cpp
@@ -364,6 +364,9 @@ bool AwsTapeMaker::tables_initialized = false;
 
 void readConfigFile(const std::string& filename, std::vector<FileConfig>& configs) {
     std::ifstream file(filename);
+    if (!file.is_open()) {
+        throw std::runtime_error("Unable to open configuration file: " + filename);
+    }
     std::string line;
     while (std::getline(file, line)) {
         std::istringstream iss(line);
